7973ebc3b4ef89857f7e1f3228198c4a20313985
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-09-09 07:28:12 +0900

    add genelized-rosenbrock-function

--- src/evolutionary_computation_benchmark_functions/core.clj
+++ src/evolutionary_computation_benchmark_functions/core.clj
@@ -14,3 +14,15 @@
        (->> x
             (map #(- (* %1 %1) (* A (Math/cos (* theta Math/PI %1)))))
             (apply +)))))
+
+(defn rosenbrock-function
+  "https://en.wikipedia.org/wiki/Rosenbrock_function
+  {\\displaystyle f(\\mathbf {x} )=\\sum _{i=1}^{N-1}[100(x_{i+1}-x_{i}^{2})^{2}+(1-x_{i})^{2}]\\quad {\\mbox{where}}\\quad \\mathbf {x} =[x_{1},\\ldots ,x_{N}]\\in \\mathbb {R} ^{N}.}[4]\n"
+  [a b x]
+  (let [x_i (drop-last x)
+        x_i1 (rest x)]
+  (->> x_i
+       (map #(+
+               (* a (Math/pow (- %  (Math/pow %2 2)) 2))
+               (Math/pow (- b %2) 2)) x_i1)
+       (apply +))))
