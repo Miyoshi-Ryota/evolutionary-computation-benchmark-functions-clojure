f560ec06aa6c429286adfbda6db6ad143622f568
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-09-09 07:07:56 +0900

    Add rastrigin function

--- src/evolutionary_computation_benchmark_functions/core.clj
+++ src/evolutionary_computation_benchmark_functions/core.clj
@@ -1,6 +1,16 @@
-(ns evolutionary-computation-benchmark-functions.core)
+(ns evolutionary-computation-benchmark-functions.core
+  (:require [clojure.spec.alpha :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn rastrigin-function
+  "`A` and `theta` is double.
+  `x` is col of double and each elements should be in range from -5.0 to 5.0."
+  [A theta x]
+  {:pre  [(s/valid? (s/coll-of number?) x)
+          (s/valid? double? A)
+          (s/valid? double? theta)]
+   :post [(s/valid? (s/and double?) %)]}
+  (let [n (count x)]
+    (+ (* A n)
+       (->> x
+            (map #(- (* %1 %1) (* A (Math/cos (* theta Math/PI %1)))))
+            (apply +)))))
