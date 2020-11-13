df64ad3ac7f6e76f8488560e5d5df3c6fb77be76
Miyoshi-Ryota <m1yosh1.ry0t4@gmail.com>
2020-11-13 15:19:17 +0900

    release 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject evolutionary-computation-benchmark-functions "0.2.0"
+(defproject evolutionary-computation-benchmark-functions "0.2.1"
   :description "A Clojure library designed to run benchmark functions which are famous in evolutionary computation groups.
   The library status is under development."
   :url "https://github.com/Miyoshi-Ryota/evolutionary-computation-benchmark-functions-clojure"
